fa31dc91acfc45910b5724d05fcf9dc0ca96bf43
amercader <amercadero@gmail.com>
2015-02-06 15:26:20 +0000

    Fix travis build script

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -26,7 +26,6 @@ cd -
 
 echo "Installing ckanext-ckanext-pdfview and its requirements..."
 python setup.py develop
-pip install -r dev-requirements.txt
 
 echo "Moving test.ini into a subdir..."
 mkdir subdir
