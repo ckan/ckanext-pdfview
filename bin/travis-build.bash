b32aea7042a4ea3d74743ede3061aa90a286b0fb
David Read <david.read@hackneyworkshop.com>
2019-11-22 14:20:31 +0000

    Modernize travis config, in case it helps tests to pass

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -18,6 +18,20 @@ else
     git checkout $CKAN_TAG
     echo "CKAN version: ${CKAN_TAG#ckan-}"
 fi
+
+# install the recommended version of setuptools
+if [ -f requirement-setuptools.txt ]
+then
+    echo "Updating setuptools..."
+    pip install -r requirement-setuptools.txt
+fi
+
+if [ $CKANVERSION == '2.7' ]
+then
+    echo "Installing setuptools"
+    pip install setuptools==39.0.1
+fi
+
 python setup.py develop
 if [ -f requirements-py2.txt ]
 then
@@ -28,26 +42,26 @@ fi
 pip install -r dev-requirements.txt
 cd -
 
-echo "Setting up Solr..."
-# Solr is multicore for tests on CKAN master now, but it's easier to run tests
-# on Travis single-core still. See https://github.com/ckan/ckan/issues/2972
-sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
-sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
-
-echo "Setting up Jetty..."
-printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
-sudo service jetty restart
-
 echo "Creating the PostgreSQL user and database..."
 sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
 sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
+sudo -u postgres psql -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
+sudo -u postgres psql -c 'CREATE DATABASE datastore_test WITH OWNER datastore_default;'
+
+echo "Setting up Solr..."
+# Solr is multicore for tests on ckan master, but it's easier to run tests on
+# Travis single-core. See https://github.com/ckan/ckan/issues/2972
+sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
+printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+sudo service jetty restart
 
 echo "Initialising the database..."
 cd ckan
 paster db init -c test-core.ini
 cd -
 
-echo "Installing ckanext-ckanext-pdfview and its requirements..."
+echo "Installing ckanext-pdfview and its requirements..."
 python setup.py develop
 
 echo "Moving test.ini into a subdir..."
