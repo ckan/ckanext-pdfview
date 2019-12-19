1a6c1ee28406456a43631f76985d24a425e5d85d
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2019-12-19 16:17:59 +0200

    format

--- ckanext/pdfview/plugin.py
+++ ckanext/pdfview/plugin.py
@@ -7,43 +7,45 @@ log = logging.getLogger(__name__)
 
 
 class PdfView(p.SingletonPlugin):
-    '''This extension views PDFs. '''
+    """This extension views PDFs. """
 
-    if not p.toolkit.check_ckan_version('2.3'):
+    if not p.toolkit.check_ckan_version("2.3"):
         raise p.toolkit.CkanVersionException(
-            'This extension requires CKAN >= 2.3. If you are using a ' +
-            'previous CKAN version the PDF viewer is included in the main ' +
-            'CKAN repository.')
+            "This extension requires CKAN >= 2.3. If you are using a "
+            + "previous CKAN version the PDF viewer is included in the main "
+            + "CKAN repository."
+        )
 
     p.implements(p.IConfigurer, inherit=True)
     p.implements(p.IConfigurable, inherit=True)
     p.implements(p.IResourceView, inherit=True)
 
-    PDF = ['pdf', 'x-pdf', 'acrobat', 'vnd.pdf']
+    PDF = ["pdf", "x-pdf", "acrobat", "vnd.pdf"]
     proxy_is_enabled = False
 
     def info(self):
-        return {'name': 'pdf_view',
-                'title': 'PDF',
-                'icon': 'file-text',
-                'default_title': 'PDF',
-                }
+        return {
+            "name": "pdf_view",
+            "title": "PDF",
+            "icon": "file-text",
+            "default_title": "PDF",
+        }
 
     def update_config(self, config):
 
-        p.toolkit.add_public_directory(config, 'theme/public')
-        p.toolkit.add_template_directory(config, 'theme/templates')
-        p.toolkit.add_resource('theme/public', 'ckanext-pdfview')
+        p.toolkit.add_public_directory(config, "theme/public")
+        p.toolkit.add_template_directory(config, "theme/templates")
+        p.toolkit.add_resource("theme/public", "ckanext-pdfview")
 
     def configure(self, config):
-        enabled = config.get('ckan.resource_proxy_enabled', False)
+        enabled = config.get("ckan.resource_proxy_enabled", False)
         self.proxy_is_enabled = enabled
 
     def can_view(self, data_dict):
-        resource = data_dict['resource']
-        format_lower = resource.get('format', '').lower()
+        resource = data_dict["resource"]
+        format_lower = resource.get("format", "").lower()
 
-        proxy_enabled = p.plugin_loaded('resource_proxy')
+        proxy_enabled = p.plugin_loaded("resource_proxy")
         same_domain = datapreview.on_same_domain(data_dict)
 
         if format_lower in self.PDF:
@@ -51,4 +53,4 @@ class PdfView(p.SingletonPlugin):
         return False
 
     def view_template(self, context, data_dict):
-        return 'pdf.html'
+        return "pdf.html"
--- ckanext/pdfview/tests/test_view.py
+++ ckanext/pdfview/tests/test_view.py
@@ -1,4 +1,5 @@
 from future import standard_library
+
 standard_library.install_aliases()
 import paste.fixture
 import pylons.config as config
@@ -17,80 +18,101 @@ except ImportError:
     # CKAN <= 2.3
     import ckan.tests as tests
 
-def _create_test_view(view_type):
-    context = {'model': model,
-               'session': model.Session,
-               'user': model.User.get('testsysadmin').name}
 
-    package = model.Package.get('annakarenina')
+def _create_test_view(view_type):
+    context = {
+        "model": model,
+        "session": model.Session,
+        "user": model.User.get("testsysadmin").name,
+    }
+
+    package = model.Package.get("annakarenina")
     resource_id = package.resources[1].id
-    resource_view = {'resource_id': resource_id,
-                     'view_type': view_type,
-                     'title': u'Test View',
-                     'description': u'A *nice* test view'}
-    resource_view = plugins.toolkit.get_action('resource_view_create')(
-        context, resource_view)
+    resource_view = {
+        "resource_id": resource_id,
+        "view_type": view_type,
+        "title": u"Test View",
+        "description": u"A *nice* test view",
+    }
+    resource_view = plugins.toolkit.get_action("resource_view_create")(
+        context, resource_view
+    )
     return resource_view, package, resource_id
 
 
 class TestPdfView(tests.WsgiAppCase):
-    view_type = 'pdf_view'
+    view_type = "pdf_view"
 
     @classmethod
     def setup_class(cls):
-        cls.config_templates = config['ckan.legacy_templates']
-        config['ckan.legacy_templates'] = 'false'
-        wsgiapp = middleware.make_app(config['global_conf'], **config)
+        cls.config_templates = config["ckan.legacy_templates"]
+        config["ckan.legacy_templates"] = "false"
+        wsgiapp = middleware.make_app(config["global_conf"], **config)
 
-        plugins.load('pdf_view')
+        plugins.load("pdf_view")
         cls.app = paste.fixture.TestApp(wsgiapp)
         cls.p = plugin.PdfView()
         cls.p.proxy_is_enabled = False
 
         create_test_data.CreateTestData.create()
 
-        cls.resource_view, cls.package, cls.resource_id = \
-            _create_test_view(cls.view_type)
+        cls.resource_view, cls.package, cls.resource_id = _create_test_view(
+            cls.view_type
+        )
 
     @classmethod
     def teardown_class(cls):
-        config['ckan.legacy_templates'] = cls.config_templates
-        plugins.unload('pdf_view')
+        config["ckan.legacy_templates"] = cls.config_templates
+        plugins.unload("pdf_view")
         model.repo.rebuild_db()
 
     def test_can_view(self):
         url_same_domain = urllib.parse.urljoin(
-            config.get('ckan.site_url', '//localhost:5000'),
-            '/resource.txt')
-        url_different_domain = 'http://some.com/resource.pdf'
+            config.get("ckan.site_url", "//localhost:5000"), "/resource.txt"
+        )
+        url_different_domain = "http://some.com/resource.pdf"
 
-        data_dict = {'resource': {'format': 'pdf', 'url': url_same_domain}}
+        data_dict = {"resource": {"format": "pdf", "url": url_same_domain}}
         assert self.p.can_view(data_dict)
 
-        data_dict = {'resource': {'format': 'x-pdf', 'url': url_same_domain}}
+        data_dict = {"resource": {"format": "x-pdf", "url": url_same_domain}}
         assert self.p.can_view(data_dict)
 
-        data_dict = {'resource': {'format': 'pdf',
-                                  'url': url_different_domain}}
+        data_dict = {
+            "resource": {"format": "pdf", "url": url_different_domain}
+        }
         assert not self.p.can_view(data_dict)
 
     def test_js_included(self):
-        url = h.url_for(controller='package', action='resource_view',
-                        id=self.package.name, resource_id=self.resource_id,
-                        view_id=self.resource_view['id'])
+        url = h.url_for(
+            controller="package",
+            action="resource_view",
+            id=self.package.name,
+            resource_id=self.resource_id,
+            view_id=self.resource_view["id"],
+        )
         result = self.app.get(url)
-        assert (('pdf_view.js' in result.body) or
-                ('pdf_view.min.js' in result.body))
+        assert ("pdf_view.js" in result.body) or (
+            "pdf_view.min.js" in result.body
+        )
 
     def test_title_description_iframe_shown(self):
-        url = h.url_for(controller='package', action='resource_read',
-                        id=self.package.name, resource_id=self.resource_id)
+        url = h.url_for(
+            controller="package",
+            action="resource_read",
+            id=self.package.name,
+            resource_id=self.resource_id,
+        )
         result = self.app.get(url)
-        assert self.resource_view['title'] in result
+        assert self.resource_view["title"] in result
         assert 'data-module="data-viewer"' in result.body
 
     def test_description_supports_markdown(self):
-        url = h.url_for(controller='package', action='resource_read',
-                        id=self.package.name, resource_id=self.resource_id)
+        url = h.url_for(
+            controller="package",
+            action="resource_read",
+            id=self.package.name,
+            resource_id=self.resource_id,
+        )
         result = self.app.get(url)
-        assert 'A <em>nice</em> test view' in result
+        assert "A <em>nice</em> test view" in result
