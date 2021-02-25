5ad953fabf0dbd2f7da9e8b6ce5c50402241ecce
pdelboca <patriciodelboca@gmail.com>
2021-02-25 13:45:39 -0300

    Refactor to use <object> tag

--- ckanext/pdfview/plugin.py
+++ ckanext/pdfview/plugin.py
@@ -1,54 +1,37 @@
-import logging
+# encoding: utf-8
 
+import logging
+from six import text_type
 import ckan.plugins as p
-import ckan.lib.datapreview as datapreview
 
 log = logging.getLogger(__name__)
+ignore_empty = p.toolkit.get_validator('ignore_empty')
 
 
-class PdfView(p.SingletonPlugin):
-    '''This extension views PDFs. '''
-
-    if not p.toolkit.check_ckan_version('2.3'):
-        raise p.toolkit.CkanVersionException(
-            'This extension requires CKAN >= 2.3. If you are using a ' +
-            'previous CKAN version the PDF viewer is included in the main ' +
-            'CKAN repository.')
+class PDFView(p.SingletonPlugin):
+    '''This plugin makes views of PDF resources, using an <object> tag'''
 
     p.implements(p.IConfigurer, inherit=True)
-    p.implements(p.IConfigurable, inherit=True)
     p.implements(p.IResourceView, inherit=True)
 
-    PDF = ['pdf', 'x-pdf', 'acrobat', 'vnd.pdf']
-    proxy_is_enabled = False
+    def update_config(self, config):
+        p.toolkit.add_template_directory(config, 'theme/templates')
 
     def info(self):
         return {'name': 'pdf_view',
-                'title': 'PDF',
-                'icon': 'file-text',
-                'default_title': 'PDF',
+                'title': p.toolkit._('PDF'),
+                'icon': 'file-pdf-o',
+                'schema': {'pdf_url': [ignore_empty, text_type]},
+                'iframed': False,
+                'always_available': False,
+                'default_title': p.toolkit._('PDF'),
                 }
 
-    def update_config(self, config):
-
-        p.toolkit.add_public_directory(config, 'theme/public')
-        p.toolkit.add_template_directory(config, 'theme/templates')
-        p.toolkit.add_resource('theme/public', 'ckanext-pdfview')
-
-    def configure(self, config):
-        enabled = config.get('ckan.resource_proxy_enabled', False)
-        self.proxy_is_enabled = enabled
-
     def can_view(self, data_dict):
-        resource = data_dict['resource']
-        format_lower = resource.get('format', '').lower()
-
-        proxy_enabled = p.plugin_loaded('resource_proxy')
-        same_domain = datapreview.on_same_domain(data_dict)
-
-        if format_lower in self.PDF:
-            return same_domain or proxy_enabled
-        return False
+        return (data_dict['resource'].get('format', '').lower() == 'pdf')
 
     def view_template(self, context, data_dict):
-        return 'pdf.html'
+        return 'pdf_view.html'
+
+    def form_template(self, context, data_dict):
+        return 'pdf_form.html'
